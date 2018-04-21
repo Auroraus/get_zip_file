3507f07839828ab0b4c1ed245408f73d5f40aaf0
zf <33646490+hfutzf@users.noreply.github.com>
2018-04-21 10:10:36 +0800

    Create zipfile.py
    压缩制定文件

---
+++ zipfile.py
@@ -0,0 +1,13 @@
+import zipfile
+import os
+def zipf(startdir):
+    file_news='F:\\usb1\\data.zip'# 压缩后文件夹的名字
+    z = zipfile.ZipFile(file_news,'w',zipfile.ZIP_DEFLATED) #参数一：文件夹名
+    for dirpath, dirnames, filenames in os.walk(startdir):
+        fpath = dirpath.replace(startdir,'') #这一句很重要，不replace的话，就从根目录开始复制
+        fpath = fpath and fpath + os.sep or ''#这句话理解我也点郁闷，实现当前文件夹以及包含的所有文件的压缩
+        for filename in filenames:
+            z.write(os.path.join(dirpath, filename),fpath+filename)
+            print ('压缩成功')
+    z.close()
+zipf('F:\\usb1\\data')
